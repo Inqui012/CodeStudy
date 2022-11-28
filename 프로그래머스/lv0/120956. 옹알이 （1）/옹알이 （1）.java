fe9195118f574e7584a0b733b7ed9d658d159d9f
Euna Jung <xii0712@gmail.com>
2022-11-28 18:23:13 +0900

    [level 0] Title: 옹알이 (1), Time: 0.52 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(int i = 0; i < babbling.length; i++){
+        	babbling[i] = babbling[i].replaceAll("aya", " ");
+        	babbling[i] = babbling[i].replaceAll("ye", " ");
+        	babbling[i] = babbling[i].replaceAll("woo", " ");
+        	babbling[i] = babbling[i].replaceAll("ma", " ");
+            babbling[i] = babbling[i].trim();
+            if(babbling[i].equals("")){
+                answer++;
+            }          
+        }
+        return answer;
+    }
+}
