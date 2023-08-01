5e5618dfc31061dea8f479e4dcceeaf566325dec
Euna Jung <xii0712@gmail.com>
2023-08-01 22:11:16 +0900

    [unrated] Title: 문자열 섞기, Time: 9.52 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(String str1, String str2) {
+        char[] char1 = str1.toCharArray();
+        char[] char2 = str2.toCharArray();
+        String answer = "";
+        for(int i = 0; i < char1.length; i++){
+            answer += char1[i];
+            answer += char2[i];
+        }
+        return answer;
+    }
+}
