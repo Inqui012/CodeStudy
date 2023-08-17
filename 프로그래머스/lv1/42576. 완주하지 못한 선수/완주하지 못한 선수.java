e72a137fedaaf7c6897041fcc82fa682306c4023
Euna Jung <xii0712@gmail.com>
2023-08-17 23:05:15 +0900

    Update 완주하지 못한 선수.java

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,8 +1,16 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
+//	효율성 고려없이 정답만 찾은경우
+//      for(String part : participant) {
+//			if(!Arrays.asList(completion).contains(part) || Collections.frequency(Arrays.asList(participant), part) != Collections.frequency(Arrays.asList(completion), part)) {
+//				answer = part;
+//			}
+//		}
         HashMap<String, Integer> retire = new HashMap<>();
 		for(String part : participant) {
+//			Collections 돌아가는게 시간을 꽤 잡아먹나보네...
+//			retire.put(part, Collections.frequency(Arrays.asList(participant), part));
 			if(retire.containsKey(part)) retire.replace(part, retire.get(part) + 1);
 			else retire.put(part, 1);
 		}
@@ -14,6 +22,5 @@ class Solution {
 			}
 		}
         return retire.keySet().toString().replace("[", "").replace("]", "");
-        
     }
 }
